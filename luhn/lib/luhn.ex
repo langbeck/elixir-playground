d87febe22480c8fe5368095ca6f500946282790f
Dórian Langbeck <langbeck@hpe.com>
2018-06-07 17:43:51 -0300

    [luhn] Add verify code and tests

--- luhn/lib/luhn.ex
+++ luhn/lib/luhn.ex
@@ -1,26 +1,42 @@
 defmodule Luhn do
 
-  def generate(n) when is_binary(n), do: gen(String.to_charlist(n), String.length(n))
-  def generate(n) when is_list(n), do: gen(n, length(n))
+  def verify(n) when is_binary(n), do: verify(String.to_charlist(n), String.length(n))
+  def verify(n) when is_list(n), do: verify(n, length(n))
 
   # Check if need to start doubling the digit
-  defp gen(n, size), do: gen(n, rem(size, 2) == 1, 0)
+  defp verify(n, size), do: calc(n, :verify, rem(size, 2) == 0, 0)
+
+  def generate(n) when is_binary(n), do: generate(String.to_charlist(n), String.length(n))
+  def generate(n) when is_list(n), do: generate(n, length(n))
+
+  # Check if need to start doubling the digit
+  defp generate(n, size), do: calc(n, :generate, rem(size, 2) == 1, 0)
 
   # No more digits, just compute the remainder
-  defp gen([], false, acc) do
+  defp calc([], :generate, false, acc) do
     case rem(acc, 10) do
       0 -> 0
       n -> 10 - n
     end
   end
 
+  defp calc([], :verify, true, acc) do
+    case rem(acc, 10) do
+      0 -> :ok
+      _ -> {:error, :invalid}
+    end
+  end
+
   # Not a valid digit
-  defp gen([n | _], _, _) when n not in ?0..?9, do: :error
+  defp calc([n | _], type, _, _) when n not in ?0..?9, do: bad_digit_error(type)
 
   # Accumulate the sum digits
-  defp gen([n | tail], false, acc), do: gen(tail,  true, acc + (n - ?0))
-  defp gen([n | tail],  true, acc), do: gen(tail, false, acc + double_it(n - ?0))
+  defp calc([n | tail], type, false, acc), do: calc(tail, type,  true, acc + (n - ?0))
+  defp calc([n | tail], type,  true, acc), do: calc(tail, type, false, acc + double_it(n - ?0))
 
   defp double_it(n) when n < 5, do: (n * 2)
   defp double_it(n),            do: (n * 2) - 9
+
+  defp bad_digit_error(:verify),   do: {:error, :format}
+  defp bad_digit_error(:generate), do: :error
 end
--- luhn/test/luhn_test.exs
+++ luhn/test/luhn_test.exs
@@ -2,50 +2,85 @@ defmodule LuhnTest do
   use ExUnit.Case
   doctest Luhn
 
-  test "well formatted cases" do
-    test_luhn_generate [
+  test "generate using well formatted inputs" do
+    test_luhn_generate([
       {"37828224631000", 5},
       {"37144963539843", 1},
       {"37873449367100", 0},
       {"3056930902590", 4},
       {"7992739871", 3},
-    ]
+    ])
   end
 
-  test "invalid digits" do
-    test_luhn_generate [
-      {".123456", :error},
-      {"123456.", :error},
-      {"-123456", :error},
-      {"123456-", :error},
-      {"a123456", :error},
-      {"1a23456", :error},
-      {"12a3456", :error},
-      {"123a456", :error},
-      {"1234a56", :error},
-      {"12345a6", :error},
-      {"123456a", :error},
-    ]
+  test "verify using well formatted valid inputs" do
+    test_luhn_verify([
+      {"378282246310005", :ok},
+      {"371449635398431", :ok},
+      {"378734493671000", :ok},
+      {"30569309025904", :ok},
+      {"79927398713", :ok},
+    ])
   end
 
-  test "with spaces" do
-    test_luhn_generate [
-      {" 123456", :error},
-      {"1 23456", :error},
-      {"12 3456", :error},
-      {"123 456", :error},
-      {"1234 56", :error},
-      {"12345 6", :error},
-      {"123456 ", :error},
+  test "verify using well formatted invalid inputs" do
+    test_luhn_verify([
+      {"378282246310006", {:error, :invalid}},
+      {"371449635398430", {:error, :invalid}},
+      {"378734493671009", {:error, :invalid}},
+      {"30569309025903", {:error, :invalid}},
+      {"79927398711", {:error, :invalid}},
+    ])
+  end
+
+  test "generate using invalid digits" do
+    invalid_digits()
+    |> Enum.zip(Stream.repeatedly(fn-> :error end))
+    |> test_luhn_generate()
+  end
+
+  test "verify using invalid digits" do
+    invalid_digits()
+    |> Enum.zip(Stream.repeatedly(fn-> {:error, :format} end))
+    |> test_luhn_verify()
+  end
+
+  defp invalid_digits() do
+    [
+      ".123456",
+      "123456.",
+      "-123456",
+      "123456-",
+      "a123456",
+      "1a23456",
+      "12a3456",
+      "123a456",
+      "1234a56",
+      "12345a6",
+      "123456a",
+
+      # Spaces
+      " 123456",
+      "1 23456",
+      "12 3456",
+      "123 456",
+      "1234 56",
+      "12345 6",
+      "123456 ",
 
       # Multiple spaces
-      {"12 4 6 ", :error},
+      "12 4 6 ",
     ]
   end
 
   defp test_luhn_generate(cases) do
     cases
     |> Stream.map(fn {n, dig} -> assert Luhn.generate(n) == dig end)
+    |> Stream.run()
+  end
+
+  defp test_luhn_verify(cases) do
+    cases
+    |> Stream.map(fn {n, status} -> assert Luhn.verify(n) == status end)
     |> Stream.run()
   end
 end
